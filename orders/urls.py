b23b8e8121574a96fb8bbc64d7e34e5eddb801f3
wuwu6249 <wuwu6249@gmail.com>
2025-12-15 00:47:22 +0800

    更新購物車與商家查看訂單功能

--- orders/urls.py
+++ orders/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('create/<int:product_id>/', views.create_order, name='create_order'),
     path('mine/', views.my_orders, name='my_orders'),
     path('seller/', views.seller_dashboard, name='seller_dashboard'),
+    path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/', views.view_cart, name='view_cart'),
+    path('cart/checkout/', views.checkout_cart, name='checkout_cart'),
 ]
